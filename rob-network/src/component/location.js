5b37e1e0ab7d5faa2e20da4f5692c359fecf8c56
Jaden Cai <67614448+gitdevhp@users.noreply.github.com>
2023-10-12 11:39:22 -0500

    yo

---
+++ rob-network/src/component/location.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+export default function loc() {
+    var loc = true;
+    return (
+        <div>
+            {loc &&
+                <h4>
+                    
+                </h4>
+            }
+        </div>
+    );
+}
