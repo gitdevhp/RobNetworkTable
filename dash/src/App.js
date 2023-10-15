380f8694477adeb87a9812512ffa54532357e967
Jaden Cai <jaden.cai@outlook.com>
2023-10-15 11:41:59 -0500

    com

--- dash/src/App.js
+++ dash/src/App.js
@@ -1,17 +1,12 @@
 import './App.css';
-import Main from './main';
+import Main from './components/main';
 
 function App() {
   var curTick = 0;
   
   return (
     <div className="App">
-      <div className="Console">
-        <h5>Dashboard</h5>
-      </div>
-      <div className="grid-flex">
-        <Main tick={curTick}/>
-      </div>
+      
     </div>
   );
 }
